897dbbbe08642ce9708eed3c41aafefba9ccbc70
sahdan17 <sahdan.wira.17@gmail.com>
2026-01-08 09:01:33 +0700

    Update personel.route.js

--- routes/personel.route.js
+++ routes/personel.route.js
@@ -140,12 +140,13 @@ router.post(
 
             // log req.body to json
             console.log("Request Body:", JSON.stringify(req.body, null, 2))
-            return res.json({ message: "Debug", body: req.body })
 
             if (req.body.foto === null && personel.foto) {
-                deleteFileIfExists(personel.foto)
-                personel.foto = null
+                // deleteFileIfExists(personel.foto)
+                // personel.foto = null
+                return res.json({ message: "Foto to be deleted", body: req.body })
             }
+            return res.json({ message: "Debug", body: req.body })
             
             if (req.body.ktp === null && personel.ktp) {
                 deleteFileIfExists(personel.ktp)
