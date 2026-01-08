35ffea6ffbcabc7fde992da50fdab50a3f1632ca
sahdan17 <sahdan.wira.17@gmail.com>
2026-01-08 10:15:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const startServer = async () => {
 
         const PORT = process.env.PORT
         app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT} mantap`)
+            console.log(`Server is running on port ${PORT}`)
         })
     } catch (error) {
         console.error("Unable to connect to the database:", error)
