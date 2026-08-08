a18cabe03ebb0c9000828dacdac0612f34c4b4f4
sahdan17 <sahdan.wira.17@gmail.com>
2026-08-08 17:06:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: true,
+  credentials: true
+}))
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 const version = "/api/v2"
